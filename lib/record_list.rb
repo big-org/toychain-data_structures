86eff123b18533f6ee55b5d80a1f2e8e440e09bb
Jikku Jose <jikkujose@gmail.com>
2016-07-15 20:12:08 +0530

    Add RecordList

--- lib/record_list.rb
+++ lib/record_list.rb
@@ -1,6 +1,18 @@
 require 'set'
+require_relative './record.rb'
 
 module ToyChain
-  class RecordList < SortedSet
+  class RecordList < Array
+    def initialize(blobs:)
+      blobs.each do |blob|
+        push Record.new(blob: blob)
+      end
+    end
+
+    def push(element)
+      super element
+      sort!
+      element
+    end
   end
 end
--- test/record_list_test.rb
+++ test/record_list_test.rb
@@ -5,10 +5,20 @@ require_relative '../lib/record.rb'
 class TestRecords < Minitest::Test
   def setup
     @rl = ToyChain::RecordList
+    @blobs = [1, 'Nikhil', 'Rakesh']
   end
 
   def test_if_sorted_set
-    assert @rl.ancestors.include?(SortedSet)
+    assert @rl.ancestors.include?(Array)
+  end
+
+  def test_constructor_can_accept_blob
+    list = @rl
+      .new(blobs: @blobs)
+      .map { |r| r }
+
+    assert_equal @blobs.length, list.length
+    assert list.all? { |record| record.is_a?(ToyChain::Record) }
   end
 end
 

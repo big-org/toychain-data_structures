ff7511d975afc28ce08a969fa23b1e92a953ec45
Nikhil <nikhilkrishna@gmail.com>
2016-07-17 21:37:28 +0530

    Implementation of Record List with SortedSet

--- lib/record_list.rb
+++ lib/record_list.rb
@@ -2,17 +2,9 @@ require 'set'
 require_relative './record.rb'
 
 module ToyChain
-  class RecordList < Array
-    def initialize(blobs:)
-      blobs.each do |blob|
-        push Record.new(blob: blob)
-      end
-    end
-
-    def push(element)
-      super element
-      sort!
-      element
+  class RecordList < SortedSet 
+    def initialize blobs 
+      super  blobs.map { |blob| Record.new(blob: blob) }
     end
   end
 end

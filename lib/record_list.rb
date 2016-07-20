72bb6346c8a1e53b805d6c71de508abd9515320d
Jikku Jose <jikkujose@gmail.com>
2016-07-20 22:02:50 +0530

    Reimplement push to add Record object
    Method push adds Record object constructed from
    the passed object

--- lib/record_list.rb
+++ lib/record_list.rb
@@ -2,9 +2,13 @@ require 'set'
 require_relative './record.rb'
 
 module ToyChain
-  class RecordList < SortedSet 
-    def initialize blobs 
-      super  blobs.map { |blob| Record.new(blob: blob) }
+  class RecordList < Array
+    def initialize(blobs:)
+      super blobs.map { |blob| Record.new(blob: blob) }
+    end
+
+    def push(blob)
+      super Record.new(blob: blob)
     end
   end
 end

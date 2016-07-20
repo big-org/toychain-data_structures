5e882bf5e8b31317873d61f87822d20d134d01e5
Jikku Jose <jikkujose@gmail.com>
2016-07-20 22:02:50 +0530

    Reimplement push to add Record object
    Method push adds Record object constructed from
    the passed object

--- test/record_list_test.rb
+++ test/record_list_test.rb
@@ -6,19 +6,28 @@ class TestRecords < Minitest::Test
   def setup
     @rl = ToyChain::RecordList
     @blobs = [1, 'Nikhil', 'Rakesh']
+    @blob = 'Individual Blob'
+    @record_from_blob = ToyChain::Record.new(blob: @blob)
   end
 
   def test_if_sorted_set
-    assert @rl.ancestors.include?(SortedSet)
+    assert @rl.ancestors.include?(Array)
   end
 
   def test_constructor_can_accept_blob
     blobset = @rl
-      .new(@blobs)
-      .each { |r| r }
+      .new(blobs: @blobs)
 
-    assert_equal @blobs.length, blobset.length 
+    assert_equal @blobs.length, blobset.length
     assert blobset.all? { |record| record.is_a?(ToyChain::Record) }
+  end
+
+  def test_individual_blobs_can_be_pushed
+    rl_instance = @rl
+      .new(blobs: [])
+      .push(@blob)
+
+    assert_equal @record_from_blob, rl_instance.last
   end
 end
 

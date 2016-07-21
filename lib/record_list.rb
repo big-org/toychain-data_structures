d0a01cdca51d65c84690d35ae0ea829b40649236
Jikku Jose <jikkujose@gmail.com>
2016-07-21 09:12:47 +0530

    Implement RecordList as a sorted list using Array

--- lib/record_list.rb
+++ lib/record_list.rb
@@ -1,14 +1,15 @@
-require 'set'
 require_relative './record.rb'
 
 module ToyChain
   class RecordList < Array
     def initialize(blobs:)
       super blobs.map { |blob| Record.new(blob: blob) }
+      sort!
     end
 
     def push(blob)
       super Record.new(blob: blob)
+      sort!
     end
   end
 end

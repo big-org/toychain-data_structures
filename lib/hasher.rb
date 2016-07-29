94f8dd9ff144657d74c2e3bf683d6b4f0dc68939
Jikku Jose <jikkujose@gmail.com>
2016-07-29 18:57:40 +0530

    Remove intermediate variable

--- lib/hasher.rb
+++ lib/hasher.rb
@@ -3,8 +3,7 @@ require 'digest'
 module ToyChain
   class Hasher
     def self.generate(object:, serializer:)
-      serial = serializer.serialize(object)
-      Digest::SHA256.hexdigest(serial)
+      Digest::SHA256.hexdigest(serializer.serialize(object))
     end
   end
 end

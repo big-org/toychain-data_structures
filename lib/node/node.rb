3efed31928d2f191326b5faef98d994ee0e3a8f7
rakeshbs <rakeshbs@gmail.com>
2016-08-20 17:21:02 +0530

    Added tests to test the config file

--- lib/node/node.rb
+++ lib/node/node.rb
@@ -1,11 +1,18 @@
+require 'json'
 require_relative '../message_pool.rb'
 
 module ToyChain
   class Node
-    attr_reader :message_pool
+    attr_reader :message_pool, :configuration
 
-    def initialize
+    def initialize(config_file:)
       @message_pool = MessagePool.new
+      read_config(config_file)
+    end
+
+    def read_config(file_path)
+      contents = File.open(file_path, 'r').read
+      @configuration = JSON.parse(contents)
     end
 
   end
--- test/node/node_test.rb
+++ test/node/node_test.rb
@@ -7,16 +7,24 @@ module ToyChain
     def setup
       @message_pool = MessagePool
       @node = Node
-      @n = @node.new
+      @n = @node.new(config_file: './lib/node/node_config.json')
     end
 
     def test_create
-      assert @node.new
+      assert @n
     end
 
     def test_node_check_message_pool
       assert_equal @n.message_pool.class, @message_pool
     end
+
+    def test_node_loads_configuration
+      assert @n.configuration
+    end
+
+    #def test_node_can_recieve_message
+
+    #end
 
   end
 end

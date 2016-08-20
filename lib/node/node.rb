13ff5f9e251bcc60ada6383db4ede7e3760f3971
rakeshbs <rakeshbs@gmail.com>
2016-08-20 15:37:02 +0530

    Created Node class with MessagePool attribute

--- lib/node/node.rb
+++ lib/node/node.rb
@@ -1,5 +1,13 @@
-module ToyChain 
-  class Node 
+require_relative '../message_pool.rb'
+
+module ToyChain
+  class Node
+    attr_reader :message_pool
+
+    def initialize
+      @message_pool = MessagePool.new
+    end
+
   end
-end 
-	
+end
+
--- test/node/node_test.rb
+++ test/node/node_test.rb
@@ -1,16 +1,22 @@
 require_relative '../test_helper.rb'
+require_relative '../../lib/message_pool.rb'
 require_relative '../../lib/node/node.rb'
 
-module ToyChain 
-  class TestMessage < Minitest::Test
-    def setup 
-    end 
+module ToyChain
+  class TestNode < Minitest::Test
+    def setup
+      @message_pool = MessagePool
+      @node = Node
+      @n = @node.new
+    end
 
     def test_create
-    end 
+      assert @node.new
+    end
 
     def test_node_check_message_pool
-    end 
+      assert_equal @n.message_pool.class, @message_pool
+    end
 
-  end 
-end 
+  end
+end
